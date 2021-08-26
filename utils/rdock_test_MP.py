f04903fc070178214b59d50ddad937e59b046ba1
sishida21 <ishida.sho.nm@yokohama-cu.ac.jp>
2021-08-26 15:15:26 +0900

    refactor: remove unused lines in rdock_test_MP.py

--- utils/rdock_test_MP.py
+++ utils/rdock_test_MP.py
@@ -1,14 +1,13 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import subprocess
-#import threading
 from multiprocessing import Process
 import os
-import shutil
 import time
 
+
 def docking_calculation(cmd):
-    proc = subprocess.call( cmd , shell=True)  
+    proc = subprocess.call( cmd , shell=True)  # Check: Not used. Is it ok?
     print('end in thread')
 
 #----parameter & preparation
@@ -26,7 +25,7 @@ def rdock_score(compound, score_type, target_dir, docking_num = 10):
     input_smiles = str(compound)
     
     num_cpus = docking_num # total number of docking    
-    score_name = score_type # SCORE or SCORE.INTER  ## edit by Biao Ma at 20190920
+    score_name = score_type # SCORE or SCORE.INTER  ## edit by Biao Ma at 20190920  # Check: Not used. Is it ok?
 
     target_dir = target_dir
     sdf_file = 'tmp_comp.sdf'
@@ -45,33 +44,18 @@ def rdock_score(compound, score_type, target_dir, docking_num = 10):
     try:
         if m1!= None:
             m = Chem.AddHs(m1)
-
             cid = AllChem.EmbedMolecule(m)
-            #fw.write(m)
-
             opt = AllChem.UFFOptimizeMolecule(m,maxIters=200)
             print('3D structure optimization by AllChem.UFFOptimizeMolecule(m,maxIters=200) :', opt)
-
             fw.write(m)
             fw.close()
 
             #----rdock calculation
-            #cmd = '/home/terayama/rdock/fiexed_side_chain/build/exe/rbdock -r cavity.prm -p /home/terayama/rdock/fiexed_side_chain/data/scripts/dock.prm -i ' + sdf_file + ' -o ' + docking_result_file + ' -T 0 -n' + str(num_docking_unit)
-            #proc = subprocess.call( cmd , shell=True)
-            #proc = subprocess.popen(cmd, shell=True)
-            #r1 = p1.wait()
-            #print('r1', p1)
-            
             start_time = time.time()
             processes = []
             for i in range(num_cpus):
-                #cmd = '/home/terayama/rdock/fiexed_side_chain/build/exe/rbdock -r cavity.prm -p /home/terayama/rdock/fiexed_side_chain/data/scripts/dock.prm -i ' + sdf_file + ' -o ' + docking_result_file + str(i) + ' -T 0 -s '+str(i)+' -n 1'
-                #cmd = 'rbdock -r cavity.prm -p /home/mki/SideEffectPrediction/08_rDock/prm/dock.prm -i ' + sdf_file + ' -o ' + docking_result_file + str(i) + ' -T 0 -s '+str(i)+' -n 1' 
                 cmd = RBT_ROOT + '/bin/rbdock -allH -r '+ target_dir + '/cavity.prm -p '+ RBT_ROOT + '/data/scripts/dock.prm -i ' + sdf_file + ' -o ' + docking_result_file + str(i) + ' -T 0 -s '+str(i)+' -n 1' 
                 print('cmd', cmd)
-                #t = threading.Thread(target=docking_calculation, args=(cmd,))
-                #threads.append(t)
-                #t.start()
                 t = Process(target=docking_calculation, args=(cmd,))
                 processes.append(t)
             for p in processes:
@@ -81,9 +65,7 @@ def rdock_score(compound, score_type, target_dir, docking_num = 10):
                 p.join()
             print('end')
             end_time = time.time()
-            #print('time used:%s' % end_time - start_time)
             print('docking time_used', end_time - start_time)
-
 
             for i in range(num_cpus):
                 #----find the minimum score of rdock from multiple docking results
@@ -118,8 +100,6 @@ def rdock_score(compound, score_type, target_dir, docking_num = 10):
                         score_inter_line = line_count + 1
                     
                     line_count += 1
-
-
             print('minimum rdock score', min_score, 'score_inter', min_score_inter)
     except:
         print('error')
@@ -127,9 +107,5 @@ def rdock_score(compound, score_type, target_dir, docking_num = 10):
         min_score_inter = 10**10
 
     os.chdir(path)
-    #os.mkdir('out') # creat the directory for rdock best output for new compound.
     best_docking_id = min_score_id if score_type == 'SCORE' else min_score_inter_id
     return [min_score, min_score_inter, best_docking_id]
-
-
-#rdock_score('Oc1ccc(NC(=O)C)cc1', 2, '<SCORE.INTER>', docking_num = 1, protein_target = 'kith')
