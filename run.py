54abc2bb82881d4329fb22b861ee4c70872f94fd
sishida21 <ishida.sho.nm@yokohama-cu.ac.jp>
2021-08-27 13:39:03 +0900

    refactor: improve readability of run.py

--- run.py
+++ run.py
@@ -13,53 +13,52 @@ from utils.make_smiles import zinc_data_with_bracket_original, zinc_processed_wi
 
 class chemical:
     def __init__(self):
-
-        self.position=['&']
-        self.num_atom=8
+        self.position = ['&']
+        self.num_atom = 8
         self.vl = ['\n', '&', 'C', '1', 'N', '[C@@H]', '2', '[C@H]', '(', '=', 'O', ')', 'S', 'c', '[S@]', '[nH]', '[O-]', '[N+]', 'n', 'F', '#', '[C@]', '[C@@]', '[S@@]', 'P', '/', '\\', 'Cl', 's', 'Br', 'o', '[NH3+]', 'I', '[n+]', '[nH+]', '3', '[N-]', '[S-]', 'B', '4', '5', '[NH+]', '[Si]', '[P@]', '[NH2+]', '[P@@]', '[N@+]', '6', '[N@@+]', '[S@@+]', '7', '8', '[P@@H]', '[n-]', '[C-]', '[P+]', '[Cu]', '[Ni]', '[Zn]', '[Au-]', '[OH+]']
         
     def Clone(self):
-
         st = chemical()
-        st.position= self.position[:]
+        st.position = self.position[:]
         return st
 
-    def SelectPosition(self,m):
+    def SelectPosition(self, m):
         self.position.append(m)
 
     def Getatom(self):
         return [i for i in range(self.num_atom)]
 
 class Node:
-    def __init__(self, position = None,  parent = None, state = None):
+    def __init__(self, position=None, parent=None, state=None):
         self.position = position
         self.parentNode = parent
         self.childNodes = []
-        self.child=None
+        self.child = None
         self.wins = 0
         self.visits = 0
-        self.nonvisited_atom=state.Getatom()
-        self.type_node=[]
-        self.depth=0
+        self.nonvisited_atom = state.Getatom()
+        self.type_node = []
+        self.depth = 0
 
 
     def Selectnode(self):
         ucb=[]
         print('UCB:')
         for i in range(len(self.childNodes)):
-            ucb_tmp = self.childNodes[i].wins/self.childNodes[i].visits+ c_val*sqrt(2*log(self.visits)/self.childNodes[i].\
-visits)
+            ucb_tmp = (self.childNodes[i].wins / self.childNodes[i].visits
+                + c_val * sqrt(2 * log(self.visits) / self.childNodes[i].visits)
+                )
             ucb.append(ucb_tmp)
-            print(self.childNodes[i].position, ucb_tmp,) 
+            print(f"{self.childNodes[i].position} {ucb_tmp}") 
         m = np.amax(ucb)
         indices = np.nonzero(ucb == m)[0]
-        ind=pr.choice(indices)
-        s=self.childNodes[ind]
-        print('\n', 'index', ind, self.position, m,) 
+        ind = pr.choice(indices)
+        s = self.childNodes[ind]
+        print(f"\nindex {ind} {self.position} {m}") 
         return s
 
     def Addnode(self, m, s):
-        n = Node(position = m, parent = self, state = s)
+        n = Node(position=m, parent=self, state=s)
         self.childNodes.append(n)
 
     def simulation(self):
@@ -75,36 +74,34 @@ def MCTS(root, verbose = False):
     start_time = time.time()
     #run_time = time.time() + 3600*hours # 3600*24
     run_time = time.time() + 60
-    rootnode = Node(state = root)
+    rootnode = Node(state=root)
     state = root.Clone()
-    """----------------------------------------------------------------------"""
-
 
     """global variables used for save valid compounds and simulated compounds"""
-    valid_compound=[]
-    depth=[]
-    min_score=1000
-    score_distribution=[]
-    min_score_distribution=[]
-    
+    valid_compound = []
+    depth = []
+    min_score = 1000
+    score_distribution = []
+    min_score_distribution = []
     generated_dict = {} #dictionary of generated compounds
     dict_id = 1  ## this id used for save best docking pose.
-    """----------------------------------------------------------------------"""
+
     out_f = open(output_dir, 'a')
 
     while time.time()<=run_time:
-        node = rootnode # important !    this node is different with state / node is the tree node
-        state = root.Clone() # but this state is the state of the initialization .  too important !!!
+        node = rootnode  # important! This node is different with state / node is the tree node
+        state = root.Clone()  # but this state is the state of the initialization. Too important!
+
         """selection step"""
-        node_pool=[]
+        node_pool = []
 
         while node.childNodes!=[]:
             node = node.Selectnode()
             state.SelectPosition(node.position)
-        print("state position:,",state.position)
+        print(f"state position: {state.position}")
 
-        if len(state.position)>= 70:
-            re= -1.0
+        if len(state.position) >= 70:
+            re = -1.0
             while node != None:
                 node.Update(re)
                 node = node.parentNode
@@ -115,27 +112,25 @@ def MCTS(root, verbose = False):
                 node.Update(re)
                 node = node.parentNode
             continue
-        
-        """------------------------------------------------------------------"""
 
         """expansion step"""
-        expanded=expanded_node(model,state.position,val,loop_num_nodeExpansion)
+        expanded = expanded_node(model, state.position, val, loop_num_nodeExpansion)
         
         new_compound = []
         nodeadded = []
-        for n in range(simulation_num):
+        for _ in range(simulation_num):
             nodeadded_tmp = node_to_add(expanded, val)
-            all_posible=chem_kn_simulation(model,state.position,val,nodeadded_tmp)
-            generate_smile=predict_smile(all_posible,val)
+            all_posible = chem_kn_simulation(model,state.position, val, nodeadded_tmp)
+            generate_smile = predict_smile(all_posible, val)
             new_compound_tmp = make_input_smile(generate_smile)
             nodeadded.extend(nodeadded_tmp)
             new_compound.extend(new_compound_tmp)
-        print('nodeadded', nodeadded) 
-        print('new compound', new_compound)
-        print('generated_dict', generated_dict)
-        print('dict_id', dict_id)
+        print(f"nodeadded {nodeadded}\n"
+              f"new compound {new_compound}\n"
+              f"generated_dict {generated_dict}\n"
+              f"dict_id {dict_id}") 
         for comp in new_compound:
-            print('lastcomp', comp[-1], ' ... ',comp[-1] == '\n')
+            print(f"lastcomp {comp[-1]} ... ", comp[-1] == '\n')
         node_index,rdock_score,valid_smile,generated_dict = check_node_type(
             new_compound,
             generated_dict,
@@ -143,19 +138,18 @@ def MCTS(root, verbose = False):
             rule=rule5,
             radical=radical_check,
             hashimoto_filter=hashimoto_filter,
-            trial=trial
+            trial=trial,
         )
         valid_compound.extend(valid_smile)
         score_distribution.extend(rdock_score)
         
-        print('node', node_index, 'rdock_score', rdock_score, 'valid', valid_smile)
-        out_f.write(str(valid_smile) + ', '+ str(rdock_score)+', '+str(min_score)+', '+str(len(state.position))+', '+str(time.time()-start_time))
-        out_f.write('\n')
+        print(f"node {node_index} rdock_score {rdock_score} valid {valid_smile}")
+        out_f.write(f"{valid_smile}, {rdock_score}, {min_score}, {len(state.position)}, {time.time()-start_time}\n")
         out_f.flush()
         dict_id += 1
 
-        if len(node_index)==0:
-            re=-1.0
+        if len(node_index) == 0:
+            re = -1.0
             while node != None:
                 node.Update(re)
                 node = node.parentNode
@@ -163,7 +157,7 @@ def MCTS(root, verbose = False):
             re_list = []
             atom_checked = []
             for i in range(len(node_index)):
-                m=node_index[i]
+                m = node_index[i]
                 atom = nodeadded[m]
                 
                 if atom not in atom_checked: 
@@ -180,49 +174,45 @@ def MCTS(root, verbose = False):
                 
                 score_index = 0 if score_type == 'SCORE' else 1
 
-                print("current minmum score",min_score)
-                if rdock_score[i][score_index]<=min_score:
+                print(f"current minmum score: {min_score}")
+                if rdock_score[i][score_index] <= min_score:
                     min_score_distribution.append(rdock_score[i][score_index])
-                    min_score=rdock_score[i][score_index]
+                    min_score = rdock_score[i][score_index]
                 else:
                     min_score_distribution.append(min_score)
+
                 """simulation"""
-                
                 if atom == '\n':
                     re = -1
                 else:
-                    re=(- (rdock_score[i][score_index] - base_rdock_score)*0.1)/(1+abs(rdock_score[i][score_index] -base_rdock_score)*0.1)
+                    re = ((-(rdock_score[i][score_index] - base_rdock_score) * 0.1)
+                        / (1 + abs(rdock_score[i][score_index] - base_rdock_score) * 0.1))
                 re_list.append(re)
-                print('atom', atom, 're_list', re_list)
-                """backpropation step"""
+                print(f"atom: {atom} re_list: {re_list}")
 
+            """backpropation step"""
             for i in range(len(node_pool)):
-
-                node=node_pool[i]
+                node = node_pool[i]
                 while node != None:
                     node.Update(re_list[i])
                     node = node.parentNode
             
             for child in node_pool:
                 print(child.position, child.wins, child.visits)
-            
-
     out_f.close()
                     
     """check if found the desired compound"""
-    print("rdock_score",score_distribution)
-    print("num valid_compound:",len(valid_compound))
-    print("valid compounds",valid_compound)
-    print("depth",depth)
-    print("min_score",min_score_distribution)
-
+    print(f"rdock_score: {score_distribution}\n"
+          f"num valid_compound: {len(valid_compound)}\n"
+          f"valid compounds: {valid_compound}\n"
+          f"depth: {depth}\n"
+          f"min_score: {min_score_distribution}")
     return valid_compound
 
 
 def UCTchemical():
     state = chemical()
-    best = MCTS(root = state,verbose = False)
-
+    best = MCTS(root=state, verbose=False)
     return best
 
 
@@ -231,9 +221,8 @@ if __name__ == "__main__":
     argvs = sys.argv
 
     """read yaml file for configuration"""
-    f = open(str(argvs[1]), "r+")
-    conf = yaml.load(f, Loader=yaml.SafeLoader)
-    f.close()
+    with open(str(argvs[1]), "r+") as f:
+        conf = yaml.load(f, Loader=yaml.SafeLoader)
 
     trial = conf.get('trial', 1)
     c_val = conf.get('c_val', 1.0)
@@ -251,32 +240,30 @@ if __name__ == "__main__":
     base_rdock_score = conf.get('base_rdock_score', -20)
     model_name = conf.get('model_name', 'model')
 
-    print('========== display configuration ==========')
-    print('trial num is: ', trial)
-    print('c_val: ', c_val)
-    print('loop_num_nodeExpansion: ', loop_num_nodeExpansion)
-    print('target: ', target)
-    print('target_dir: ',target_dir)
-    print('max run time: ',hours)
-    print('score_type: ', score_type)
-    print('docking_num: ',docking_num)
-    print('sa_threshold: ',sa_threshold)
-    print('model_name: ', model_name)
-    print('base_rdock_score: ', base_rdock_score)
-    print('simulation_num: ',simulation_num)
-    print('hashimoto_filter: ', hashimoto_filter)
-    """----------------------------------------------------------------------"""
+    print(f"========== Configuration ==========\n"
+          f"trial num is: {trial}\n"
+          f"c_val: {c_val}\n"
+          f"loop_num_nodeExpansion: {loop_num_nodeExpansion}\n"
+          f"target: {target}\n"
+          f"target_dir: {target_dir}\n"
+          f"max run time: {hours}\n"
+          f"score_type: {score_type}\n"
+          f"docking_num: {docking_num}\n"
+          f"sa_threshold: {sa_threshold}\n"
+          f"model_name: {model_name}\n"
+          f"base_rdock_score: {base_rdock_score}\n"
+          f"simulation_num: {simulation_num}\n"
+          f"hashimoto_filter: {hashimoto_filter}")
 
-    output_dir = 'result_'+target+'_C'+str(c_val)+'_trial'+str(trial)+'.txt'
+    output_dir = f"result_{target}_C{c_val}_trial{trial}.txt"
 
     smile_old=zinc_data_with_bracket_original('data/250k_rndm_zinc_drugs_clean.smi')
     val,smile=zinc_processed_with_bracket(smile_old)
-    print('val is ', val)
+    print(f"val is {val}")
 
     out_f = open(output_dir, 'w')
-    out_f.write('#valid_smile, rdock_score, min_score, depth, used_time')
-    out_f.write('\n')
-    out_f.close()
+    with open(output_dir, 'w') as f:
+        f.write('#valid_smile, rdock_score, min_score, depth, used_time\n')
 
-    model=loaded_model('model/'+ model_name)  #WM300 not tested  
-    valid_compound=UCTchemical()
+    model = loaded_model('model/' + model_name)  #WM300 not tested  
+    valid_compound = UCTchemical()
