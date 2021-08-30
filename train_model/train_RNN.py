1827f743491a95e69dbecb89acf282a6ba624b33
sishida21 <kurosuke1021@gmail.com>
2021-08-30 14:41:46 +0900

    refactor: delete unused methods in train_RNN.py

--- train_model/train_RNN.py
+++ train_model/train_RNN.py
@@ -18,110 +18,6 @@ sys.path.append("../")
 from utils.make_smiles import zinc_data_with_bracket_original, zinc_processed_with_bracket
 
 
-def load_data():
-    sen_space=[]
-    f = open(dataset, 'rb')  # TODO: dataset should be an argument
-    reader = csv.reader(f)
-    for row in reader:
-        sen_space.append(row)
-    f.close()
-
-    element_table=["Cu","Ti","Zr","Ga","Ge","As","Se","Br","Si","Zn","Cl","Be","Ca","Na","Sr","Ir","Li","Rb","Cs","Fr","Be","Mg",
-            "Ca","Sr","Ba","Ra","Sc","La","Ac","Ti","Zr","Nb","Ta","Db","Cr","Mo","Sg","Mn","Tc","Re","Bh","Fe","Ru","Os","Hs","Co","Rh",
-            "Ir","Mt","Ni","Pd","Pt","Ds","Cu","Ag","Au","Rg","Zn","Cd","Hg","Cn","Al","Ga","In","Tl","Nh","Si","Ge","Sn","Pb","Fl",
-            "As","Sb","Bi","Mc","Se","Te","Po","Lv","Cl","Br","At","Ts","He","Ne","Ar","Kr","Xe","Rn","Og"]
-    word1=sen_space[0]
-    word_space=list(word1[0])
-    end="\n"
-    word_space.append(end)
-    all_smile=[]
-
-    for i in range(len(sen_space)):
-        word1=sen_space[i]
-        word_space=list(word1[0])
-        word=[]
-        j=0
-        while j<len(word_space):
-            word_space1=[]
-            word_space1.append(word_space[j])
-            if j+1<len(word_space):
-                word_space1.append(word_space[j+1])
-                word_space2=''.join(word_space1)
-            else:
-                word_space1.insert(0,word_space[j-1])
-                word_space2=''.join(word_space1)
-            if word_space2 not in element_table:
-                word.append(word_space[j])
-                j=j+1
-            else:
-                word.append(word_space2)
-                j=j+2
-
-        word.append(end)
-        all_smile.append(list(word))
-    val=[]
-    for i in range(len(all_smile)):
-        for j in range(len(all_smile[i])):
-            if all_smile[i][j] not in val:
-                val.append(all_smile[i][j])
-    val.remove("\n")
-    val.insert(0,"\n")
-
-    return val, all_smile
-
-
-def organic_data():
-    sen_space=[]
-    f = open('/Users/yang/LSTM-chemical-project/make_sm.csv', 'rb')
-    reader = csv.reader(f)
-    for row in reader:
-        sen_space.append(row)
-    f.close()
-
-    element_table=["Cu","Ti","Zr","Ga","Ge","As","Se","Br","Si","Zn","Cl","Be","Ca","Na","Sr","Ir","Li","Rb","Cs","Fr","Be","Mg",
-            "Ca","Sr","Ba","Ra","Sc","La","Ac","Ti","Zr","Nb","Ta","Db","Cr","Mo","Sg","Mn","Tc","Re","Bh","Fe","Ru","Os","Hs","Co","Rh",
-            "Ir","Mt","Ni","Pd","Pt","Ds","Cu","Ag","Au","Rg","Zn","Cd","Hg","Cn","Al","Ga","In","Tl","Nh","Si","Ge","Sn","Pb","Fl",
-            "As","Sb","Bi","Mc","Se","Te","Po","Lv","Cl","Br","At","Ts","He","Ne","Ar","Kr","Xe","Rn","Og"]
-    word1=sen_space[0]
-    word_space=list(word1[0])
-    end="\n"
-    word_space.append(end)
-    all_smile=[]
-
-    for i in range(len(sen_space)):
-        word1=sen_space[i]
-        word_space=list(word1[0])
-        word=[]
-        j=0
-        while j<len(word_space):
-            word_space1=[]
-            word_space1.append(word_space[j])
-            if j+1<len(word_space):
-                word_space1.append(word_space[j+1])
-                word_space2=''.join(word_space1)
-            else:
-                word_space1.insert(0,word_space[j-1])
-                word_space2=''.join(word_space1)
-            if word_space2 not in element_table:
-                word.append(word_space[j])
-                j=j+1
-            else:
-                word.append(word_space2)
-                j=j+2
-
-        word.append(end)
-        all_smile.append(list(word))
-    val=[]
-    for i in range(len(all_smile)):
-        for j in range(len(all_smile[i])):
-            if all_smile[i][j] not in val:
-                val.append(all_smile[i][j])
-    val.remove("\n")
-    val.insert(0,"\n")
-
-    return val, all_smile
-
-
 def prepare_data(smiles,all_smile):
     all_smile_index=[]
     for i in range(len(all_smile)):
@@ -141,22 +37,6 @@ def prepare_data(smiles,all_smile):
     return X_train,y_train
 
 
-def generate_smile(model,val):
-    end="\n"
-    start_smile_index= [val.index("C")]
-    new_smile=[]
-
-    while not start_smile_index[-1] == val.index(end):
-        predictions=model.predict(start_smile_index)
-        smf=[]
-        for i in range (len(X)):
-            sm=[]
-            for j in range(len(X[i])):
-                sm.append(np.argmax(predictions[i][j]))
-            smf.append(sm)
-        new_smile.append(sampled_word)  # Check: sample_word is not defined
-
-
 def save_model(model):
     model_json = model.to_json()
     with open(output_json, "w") as json_file:
@@ -170,6 +50,7 @@ def save_model_ES(model):
     with open(output_json, "w") as json_file:
         json_file.write(model_json)
     print("Saved model to disk")
+
 
 if __name__ == "__main__":
     argvs = sys.argv
