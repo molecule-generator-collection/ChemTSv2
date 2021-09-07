5e17a9864192129d954d1dfd6c113cc13546f82b
sishida21 <kurosuke1021@gmail.com>
2021-09-08 07:42:01 +0900

    feat: add an argument for specifying gpu devices

--- utils/load_model.py
+++ utils/load_model.py
@@ -1,6 +1,4 @@
-import os
 from tensorflow.keras.models import model_from_json
-os.environ["CUDA_VISIBLE_DEVICES"]= "-1" #"0,1,2,3,"                                                                                                                  
 
 
 def loaded_model(model_json, model_weight, logger):
