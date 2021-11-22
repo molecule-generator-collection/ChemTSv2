2dcfe12ff63f15be85f244af28406645a9ec85ed
tyszw <ttyyszw@gmail.com>
2021-11-22 10:24:48 +0900

    feat: update objective functions in dscore_reward.py

--- reward/dscore_reward.py
+++ reward/dscore_reward.py
@@ -1,25 +1,27 @@
 import os
 import pickle
 import sys
-import traceback
 
 from rdkit import Chem
 from rdkit.Chem import AllChem, RDConfig
-import numpy as np
 sys.path.append(os.path.join(RDConfig.RDContribDir, 'SA_Score'))
 import sascorer
 
 from misc.scaler import minmax, max_gauss, min_gauss
 
-EGFR_MODEL_PATH = 'data/model/lgb_egfr.pickle'
-BACE1_MODEL_PATH = 'data/model/lgb_bace1.pickle'
+LGB_MODELS_PATH = 'data/model/lgb_models.pickle'
+with open(LGB_MODELS_PATH, mode='rb') as f:
+    lgb_models = pickle.load(f)
 
-with open(EGFR_MODEL_PATH, mode='rb') as f1, \
-     open(BACE1_MODEL_PATH, mode='rb') as f2:
-        lgb_egfr = pickle.load(f1)
-        print(f"[INFO] loaded model from {EGFR_MODEL_PATH}")
-        lgb_bace1 = pickle.load(f2)
-        print(f"[INFO] loaded model from {BACE1_MODEL_PATH}")
+
+def scale_objective_values(target_name, value, conf):
+    scaling = conf["scaling_function"]
+    if scaling[target_name] == "max_gauss":
+        return max_gauss(value)
+    elif scaling[target_name] == "min_gauss":
+        return min_gauss(value)
+    else:
+        return None
 
 
 def get_objective_functions(conf):
@@ -27,13 +29,79 @@ def get_objective_functions(conf):
         if mol is None:
             return None
         fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
-        return lgb_egfr.predict(fp)[0]
+        return lgb_models["EGFR"].predict(fp, num_iteration=lgb_models["EGFR"].best_iteration)[0]
     
     def BACE1(mol):
         if mol is None:
             return None
         fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
-        return lgb_bace1.predict(fp)[0]
+        return lgb_models["BACE1"].predict(fp, num_iteration=lgb_models["BACE1"].best_iteration)[0]
+
+    def ERBB2(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["ERBB2"].predict(fp, num_iteration=lgb_models["ERBB2"].best_iteration)[0]
+
+    def ABL(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["ABL"].predict(fp, num_iteration=lgb_models["ABL"].best_iteration)[0]
+
+    def SRC(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["SRC"].predict(fp, num_iteration=lgb_models["SRC"].best_iteration)[0]
+
+    def LCK(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["LCK"].predict(fp, num_iteration=lgb_models["LCK"].best_iteration)[0]
+
+    def PDGFRbeta(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["PDGFRbeta"].predict(fp, num_iteration=lgb_models["PDGFRbeta"].best_iteration)[0]
+
+    def VEGFR2(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["VEGFR2"].predict(fp, num_iteration=lgb_models["VEGFR2"].best_iteration)[0]
+
+    def FGFR1(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["FGFR1"].predict(fp, num_iteration=lgb_models["FGFR1"].best_iteration)[0]
+
+    def EPHB4(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["EPHB4"].predict(fp, num_iteration=lgb_models["EPHB4"].best_iteration)[0]
+
+    def Solubility(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["Sol"].predict(fp, num_iteration=lgb_models["Sol"].best_iteration)[0]
+
+    def Permeability(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["Perm"].predict(fp, num_iteration=lgb_models["Perm"].best_iteration)[0]
+
+    def Toxicity(mol):
+        if mol is None:
+            return None
+        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+        return lgb_models["Tox"].predict(fp, num_iteration=lgb_models["Tox"].best_iteration)[0]
 
     def SAScore(mol):
         return sascorer.calculateScore(mol)
@@ -44,31 +112,26 @@ def get_objective_functions(conf):
         except Chem.rdchem.AtomValenceException:
             return None
 
-    return [EGFR, BACE1, SAScore, QED]
+    return [EGFR, BACE1, ERBB2, ABL, SRC, LCK, PDGFRbeta, VEGFR2, FGFR1, EPHB4, Solubility, Permeability, Toxicity, SAScore, QED]
 
 
-def calc_reward_from_objective_values(values, conf):
-    weight = conf["weight"]
-    scaling = conf["scaling_function"]
+def calc_reward_from_objective_values(values, conf):    
     if None in values:
         return -1
-    egfr, bace1, sascore, qed = values
-    if scaling["egfr"] == "max_gauss":
-        scaled_egfr = max_gauss(egfr)
-    elif scaling["egfr"] == "min_gauss":
-        scaled_egfr = min_gauss(egfr)
-    else:
-        scaled_egfr = None
-    if scaling["bace1"] == "max_gauss":
-        scaled_bace1 = max_gauss(bace1)
-    elif scaling["bace1"] == "min_gauss":
-        scaled_bace1 = min_gauss(bace1)
-    else:
-        scaled_bace1 = None
+    scaled_values = []
+    target_names = list(conf['scaling_function'].keys())
+    affinity_values = values[:len(target_names)]
+    for n, v in zip(target_names, affinity_values):
+        scaled_values.append(scale_objective_values(n, v, conf))
+    solubility, permeability, toxicity, sascore, qed = values[len(affinity_values):]
+    scaled_values.append(max_gauss(solubility, a=1, mu=-2, sigma=0.6))
+    scaled_values.append(max_gauss(permeability, a=1, mu=-4.5, sigma=0.5))
+    scaled_values.append(min_gauss(toxicity, a=1, mu=5.5, sigma=0.5))
     # SA score is made negative when scaling because a smaller value is more desirable.
-    scaled_sascore = minmax(-1 * sascore, -10, -1)
+    scaled_values.append(minmax(-1 * sascore, -10, -1))
     # Since QED is a value between 0 and 1, there is no need to scale it.
-    scaled_values = [scaled_egfr, scaled_bace1, scaled_sascore, qed]
+    scaled_values.append(qed)
+    weight = conf["weight"]
     multiplication_value = 1
     for v, w in zip(scaled_values, weight.values()):
         multiplication_value *= v**w
