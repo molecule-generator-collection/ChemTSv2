f16fa2ae36b66ccd66fbbe00709fb914b2d3d87a
sishida21 <29348731+sishida21@users.noreply.github.com>
2022-03-04 11:29:22 +0900

    fix: temporarily deal with the errors in Autodock Vina

--- reward/Vina_reward.py
+++ reward/Vina_reward.py
@@ -14,37 +14,44 @@ def get_objective_functions(conf):
         v.set_receptor(rigid_pdbqt_filename=conf['vina_receptor'])
 
         mol = Chem.AddHs(mol)
-        AllChem.EmbedMolecule(mol)
-        mol_conf = mol.GetConformer(-1)
-        centroid = list(rdMolTransforms.ComputeCentroid(mol_conf))
-        tr = [conf['vina_center'][i] - centroid[i] for i in range(3)]
-        for i, p in enumerate(mol_conf.GetPositions()):
-            mol_conf.SetAtomPosition(i, Point3D(p[0]+tr[0], p[1]+tr[1], p[2]+tr[2]))
-        mol_pdbqt = ordkit.MolToPDBQTBlock(mol, computeCharges=True)
-        v.set_ligand_from_string(mol_pdbqt)
+        try:
+            AllChem.EmbedMolecule(mol)
+            mol_conf = mol.GetConformer(-1)
+            centroid = list(rdMolTransforms.ComputeCentroid(mol_conf))
+            tr = [conf['vina_center'][i] - centroid[i] for i in range(3)]
+            for i, p in enumerate(mol_conf.GetPositions()):
+                mol_conf.SetAtomPosition(i, Point3D(p[0]+tr[0], p[1]+tr[1], p[2]+tr[2]))
+            mol_pdbqt = ordkit.MolToPDBQTBlock(mol, computeCharges=True)
+            v.set_ligand_from_string(mol_pdbqt)
 
-        v.compute_vina_maps(
-            center=conf['vina_center'],
-            box_size=conf['vina_box_size'],
-            spacing=conf['vina_spacing'])
+            v.compute_vina_maps(
+                center=conf['vina_center'],
+                box_size=conf['vina_box_size'],
+                spacing=conf['vina_spacing'])
 
-        _ = v.optimize()
+            _ = v.optimize()
 
-        if conf['debug']:
-            pprint.pprint(v.info())
+            if conf['debug']:
+                pprint.pprint(v.info())
 
-        v.dock(
-            exhaustiveness=conf['vina_exhaustiveness'],
-            n_poses=conf['vina_n_poses'],
-            min_rmsd=conf['vina_min_rmsd'],
-            max_evals=conf['vina_max_evals'])
-        if conf['debug']:
-            print(f"Vina Docking energies: {v.energies()}")
-        return v.energies()[0][1]
+            v.dock(
+                exhaustiveness=conf['vina_exhaustiveness'],
+                n_poses=conf['vina_n_poses'],
+                min_rmsd=conf['vina_min_rmsd'],
+                max_evals=conf['vina_max_evals'])
+            if conf['debug']:
+                print(f"Vina Docking energies: {v.energies()}")
+            return v.energies()[0][1]
+        except Exception as e:
+            print(f"Error SMILES: {Chem.MolToSmiles(mol)}")
+            print(e)
+            return None
     return [VinaScore]
 
 
 def calc_reward_from_objective_values(values, conf):
     min_inter_score = values[0]
+    if min_inter_score is None:
+        return -1
     score_diff = min_inter_score - conf['vina_base_score']
     return - score_diff * 0.1 / (1 + abs(score_diff) * 0.1)
