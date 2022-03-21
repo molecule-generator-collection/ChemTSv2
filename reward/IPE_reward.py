modified reward calculation and fixed bugs in Gaussian calc.

reward/IPE_reward.py
@@ -24,11 +24,6 @@ class IPE_reward(Reward):
             return sascorer.calculateScore(mol)
         
         def IPE(mol):
-            #Check dondition of aromatic and charge
-            Naroma = Descriptors.NumAromaticRings(mol)
-            Ncharge = Chem.rdmolops.GetFormalCharge(mol)
-            if not (Naroma != 0 and Ncharge == 0):
-                return 0
 
             mol_index =  str(conf['gid'])
             print('gid', mol_index, Chem.MolToSmiles(mol))
@@ -57,6 +52,7 @@ class IPE_reward(Reward):
             option = conf['gau_option'] #"opt deen homolumo stable2o2"
             solvent = str(conf['gau_solvent'])
             error = str(conf['gau_error'])
+            current_dir = os.getcwd()
             try:
                 run_pack = GaussianRunPack.GaussianDFTRun(functional, basis, core_num, option, SDFinput_opt, solvent = solvent, error = error)
                 run_pack.mem = '12GB'
@@ -70,6 +66,7 @@ class IPE_reward(Reward):
                 outdic = {}
             
             print('outdic', outdic)
+            os.chdir(current_dir)
 
             #Post-processing
             #If Gaussian failed and did not make calc_dir, calc_dir for gaussian is required for the later process.
@@ -78,12 +75,12 @@ class IPE_reward(Reward):
                 os.mkdir(calc_dir)
                 
             result_dir = os.path.join(conf['output_dir'], 'gaussian_result')
-            if not os.path.isdir(result_dir):
-                os.mkdir(result_dir)
+            #if not os.path.isdir(result_dir):
+            #    os.mkdir(result_dir)
             
             if os.path.isdir(os.path.join(result_dir, calc_dir)):
                 shutil.rmtree(os.path.join(result_dir, calc_dir))
-            shutil.move(calc_dir, result_dir)
+            print('shutil.move(calc_dir, result_dir)', shutil.move(calc_dir, result_dir))
             shutil.move(SDFinput, os.path.join(result_dir, calc_dir))
             shutil.move(SDFinput_opt, os.path.join(result_dir, calc_dir))
             with open(os.path.join(result_dir, calc_dir, 'gaussian_result.pickle'), mode='wb') as f:
@@ -104,26 +101,27 @@ class IPE_reward(Reward):
 
 
 
-    def intensity_scaler_tanh(data, epsilon = 2):
-        return np.tanh((np.log10(10**-epsilon + data) + epsilon) )
-
-    def gauss(x, a=1, mu=0, sigma=1):
-        return a * np.exp(-(x - mu)**2 / (2*sigma**2))
-
-    def sa_scaler(v, sa_threshold = 3.5):
-        if v < sa_threshold:
-            return 1
-        else:
-            return gauss(v, mu = sa_threshold, sigma = 1)
-
-
+   
 
     def calc_reward_from_objective_values(values, conf):
+        def intensity_scaler_tanh( data, epsilon = 2):
+            return np.tanh((np.log10(10**-epsilon + data) + epsilon) )
+
+        def gauss(x, a=1, mu=0, sigma=1):
+            return a * np.exp(-(x - mu)**2 / (2*sigma**2))
+
+        def sa_scaler(v, sa_threshold = 3.5, sigma = 1):
+            if v < sa_threshold:
+                return 1
+            else:
+                return gauss(v, mu = sa_threshold, sigma = sigma)
+
+
         #minizing IPE value. Note that ipe_val = 0 when calculation failed or mol had charge.
         logP, sascore, ipe_val = values
         ipe_val = 100 if ipe_val == 0 else ipe_val
         ipe_score = 1 - 0.1*ipe_val / (1 + 0.1*ipe_val)
-        score = ipe_score * sascore
-        print(ipe_val, sascore, score, ipe_score)
+        score = ipe_score * sa_scaler(sascore, sa_threshold = 3)
+        #print(ipe_val, sascore, score, ipe_score)
 
         return score