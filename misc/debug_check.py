f77e229daa4683ed25b9458e1b437cc949e406f8
sishida21 <29348731+sishida21@users.noreply.github.com>
2022-03-28 15:32:15 +0900

    chore: add a script for checking if the result is identical to the reference result

---
+++ misc/debug_check.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def main():
+    df_ref = pd.read_csv('../result/example01/result_C1.0_for_debug.csv')
+    df = pd.read_csv('../result/example01/result_C1.0.csv')
+
+    df_ref.drop('elapsed_time', axis=1, inplace=True)
+    df.drop('elapsed_time', axis=1, inplace=True)
+
+    if df.equals(df_ref):
+        print('[INFO] OK!')
+    else:
+        print('[INFO] False, check your modifications.')
+
+
+if __name__ == "__main__":
+    main()
+
