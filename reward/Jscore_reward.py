fe27c2d1a0a66e993a747bdfbb8288240a0c9b5f
sishida21 <29348731+sishida21@users.noreply.github.com>
2022-06-17 16:32:47 +0900

    chore: update Jscore_reward.py

--- reward/Jscore_reward.py
+++ reward/Jscore_reward.py
@@ -1,28 +1,29 @@
 """ Refer to https://arxiv.org/abs/1710.00616 for Jscore """
-import os
 import sys
 
 from rdkit.Chem import Descriptors
 sys.path.append("./data/")
 import sascorer
 
+from reward.reward import Reward
 
-def get_objective_functions(conf):
-    def LogP(mol):
-        return Descriptors.MolLogP(mol)
+class Jscore_reward(Reward):
+    def get_objective_functions(conf):
+        def LogP(mol):
+            return Descriptors.MolLogP(mol)
 
-    def SAScore(mol):
-        return sascorer.calculateScore(mol)
+        def SAScore(mol):
+            return sascorer.calculateScore(mol)
 
-    def RingSizePenalty(mol):
-        ri = mol.GetRingInfo()
-        max_ring_size = max((len(r) for r in ri.AtomRings()), default=0)
-        return max_ring_size - 6
+        def RingSizePenalty(mol):
+            ri = mol.GetRingInfo()
+            max_ring_size = max((len(r) for r in ri.AtomRings()), default=0)
+            return max_ring_size - 6
 
-    return [LogP, SAScore, RingSizePenalty]
+        return [LogP, SAScore, RingSizePenalty]
 
 
-def calc_reward_from_objective_values(values, conf):
-    logP, sascore, ring_size_penalty = values
-    jscore = logP - sascore - ring_size_penalty
-    return jscore / (1 + jscore)
+    def calc_reward_from_objective_values(values, conf):
+        logP, sascore, ring_size_penalty = values
+        jscore = logP - sascore - ring_size_penalty
+        return jscore / (1 + jscore)
