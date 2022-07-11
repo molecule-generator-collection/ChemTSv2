363a3d0faf9656cba17cdde509c7e59effa5044c
tyszw <ttyyszw@gmail.com>
2022-07-11 13:27:22 +0900

    fix: update egfr_reward.py

--- reward/egfr_reward.py
+++ reward/egfr_reward.py
@@ -1,23 +1,25 @@
 import pickle
 
-from rdkit import Chem
 from rdkit.Chem import AllChem
 import numpy as np
 
-
-EGFR_MODEL_PATH = 'data/model/lgb_egfr.pickle'
-with open(EGFR_MODEL_PATH, mode='rb') as f:
-    lgb_egfr = pickle.load(f)
-    print(f"[INFO] loaded model from {EGFR_MODEL_PATH}")
-
-def get_objective_functions(conf):
-    def EGFR(mol):
-        if mol is None:
-            return None
-        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
-        return lgb_egfr.predict(fp)[0]
-    return [EGFR]
+from reward.reward import Reward
 
 
-def calc_reward_from_objective_values(values, conf):
-    return np.tanh(values[0]/10) if None not in values else -1
+LGB_MODELS_PATH = 'data/model/lgb_models.pickle'
+with open(LGB_MODELS_PATH, mode='rb') as f:
+    lgb_models = pickle.load(f)
+    print(f"[INFO] loaded model from {LGB_MODELS_PATH}")
+
+
+class EGFR_reward(Reward):
+    def get_objective_functions(conf):
+        def EGFR(mol):
+            if mol is None:
+                return None
+            fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+            return lgb_models["EGFR"].predict(fp)[0]
+        return [EGFR]
+
+    def calc_reward_from_objective_values(values, conf):
+        return np.tanh(values[0]/10) if None not in values else -1
