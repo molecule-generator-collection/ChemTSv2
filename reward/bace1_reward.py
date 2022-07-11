ce2bd3b61cd36558bb508820507fdff8d2976154
tyszw <ttyyszw@gmail.com>
2022-07-11 13:26:40 +0900

    fix: update bace1_reward.py

--- reward/bace1_reward.py
+++ reward/bace1_reward.py
@@ -1,23 +1,25 @@
 import pickle
 
-from rdkit import Chem
 from rdkit.Chem import AllChem
 import numpy as np
 
-
-BACE1_MODEL_PATH = 'data/model/lgb_bace1.pickle'
-with open(BACE1_MODEL_PATH, mode='rb') as f:
-    lgb_bace1 = pickle.load(f)
-    print(f"[INFO] loaded model from {BACE1_MODEL_PATH}")
-
-def get_objective_functions(conf):
-    def BACE1(mol):
-        if mol is None:
-            return None
-        fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
-        return lgb_bace1.predict(fp)[0]
-    return [BACE1]
+from reward.reward import Reward
 
 
-def calc_reward_from_objective_values(values, conf):
-    return np.tanh(values[0]/10) if None not in values else -1
+LGB_MODELS_PATH = 'data/model/lgb_models.pickle'
+with open(LGB_MODELS_PATH, mode='rb') as f:
+    lgb_models = pickle.load(f)
+    print(f"[INFO] loaded model from {LGB_MODELS_PATH}")
+
+
+class BACE1_reward(Reward):
+    def get_objective_functions(conf):
+        def BACE1(mol):
+            if mol is None:
+                return None
+            fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048)]
+            return lgb_models["BACE1"].predict(fp)[0]
+        return [BACE1]
+
+    def calc_reward_from_objective_values(values, conf):
+        return np.tanh(values[0]/10) if None not in values else -1
