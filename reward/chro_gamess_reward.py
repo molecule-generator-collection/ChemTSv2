9d588139cb7e599267a5aa155799f5f4386f7487
sishida21 <29348731+sishida21@users.noreply.github.com>
2023-06-21 19:19:02 +0900

    feat: add chromophore design example using GAMESS

---
+++ reward/chro_gamess_reward.py
@@ -0,0 +1,80 @@
+import os
+import pickle
+import shutil
+import sys
+
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+sys.path.append("./data/")
+import sascorer
+
+from reward.reward import Reward
+from qcforever.gamess_run import GamessRunPack
+
+
+class UV_reward(Reward):
+    def get_objective_functions(conf):
+        def SAScore(mol):
+            return sascorer.calculateScore(mol)
+        
+        def UV(mol):
+            sdf_input = f"InputMol{conf['gid']}.sdf"
+            mol_wH = Chem.AddHs(mol)
+            AllChem.EmbedMolecule(mol_wH, AllChem.ETKDG())
+            Chem.MolToMolFile(mol_wH, sdf_input)
+            try:
+                AllChem.UFFOptimizeMolecule(mol_wH, maxIters=200)
+                sdf_input_opt = f"InputMolopt_ok_{conf['gid']}.sdf"
+            except:
+                sdf_input_opt = f"InputMolopt_fail_{conf['gid']}.sdf"
+            Chem.MolToMolFile(mol_wH, sdf_input_opt)
+            
+            #Gaussian calculation
+            current_dir = os.getcwd()
+            calc_dir = sdf_input_opt.split('.')[0]
+            try:
+                run_pack = GamessRunPack.GamessDFTRun(
+                    conf['gms_functional'],
+                    conf['gms_basis'], 
+                    conf['gms_core_num'],
+                    conf['gms_option'],
+                    sdf_input_opt,
+                    error=str(conf['gms_error']))
+                run_pack.mem = conf['gms_memory']  # 5GB minimum
+                result_dict = run_pack.run_gamess()
+                run_pack = None  # 
+            except Exception as e:
+                print('Gamess DFT calculation failed:', e)
+                result_dict = {}
+            os.chdir(current_dir)
+
+            #Post-processing
+            #If Gamess failed and did not make calc_dir, calc_dir for gamess is required for the later process.
+            if not os.path.exists(calc_dir):
+                os.mkdir(calc_dir)
+            result_base_dir = os.path.join(conf['output_dir'], 'gamess_result')
+            result_dir = os.path.join(result_base_dir, calc_dir)
+            if os.path.isdir(result_dir):
+                shutil.rmtree(result_dir)
+            shutil.move(calc_dir, result_dir)
+            shutil.move(sdf_input, result_dir)
+            shutil.move(sdf_input_opt, result_dir)
+            with open(os.path.join(result_dir, 'gamess_result.pickle'), mode='wb') as f:
+                pickle.dump(result_dict, f)
+
+            if 'uv' in result_dict and len(result_dict['uv']) > 0 and result_dict['uv'][0] != []:
+                uv_abs_wl = result_dict['uv'][0][0]
+            else:
+                uv_abs_wl = 0
+            return uv_abs_wl
+
+        return [SAScore, UV]
+
+
+    def calc_reward_from_objective_values(values, conf):
+        # https://www.tandfonline.com/doi/pdf/10.1080/14686996.2022.2075240
+        sascore, uv_abs_wl = values
+        f_score = np.tanh(0.003*(uv_abs_wl-400)) / 2
+        g_score = (-np.tanh(sascore-4)+1) / 2
+        return f_score * g_score
