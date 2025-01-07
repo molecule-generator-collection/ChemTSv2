397c270df09efa20360f830246ff8e099119beb3
murakami@mnode <i180634g@gmail.com>
2025-01-07 16:35:13 +0900

    feat: Add multiple molecular property filters for molecule generation
    make nrotb_filter.py
    
    make tpsa_filter.py
    
    make hba_filter.py
    
    make logp_filter.py

---
+++ filter/hba_filter.py
@@ -0,0 +1,15 @@
+from rdkit.Chem import Descriptors
+
+from chemtsv2.filter import Filter
+from chemtsv2.utils import transform_linker_to_mol
+
+class HBAFilter(Filter):
+    def check(mol, config): 
+        return Descriptors.NumHAcceptors(mol) <= config['hba_filter']['threshold']
+    
+class HBAFilterForXMol(Filter):
+    def check(mol, conf):
+        @transform_linker_to_mol(conf)
+        def _check(mol, conf):
+            return HBAFilter.check(mol, conf)
+        return _check(mol, conf)
---
+++ filter/logp_filter.py
@@ -0,0 +1,15 @@
+from rdkit.Chem import Descriptors
+
+from chemtsv2.filter import Filter
+from chemtsv2.utils import transform_linker_to_mol
+
+class LogPFilter(Filter):
+    def check(mol, config): 
+        return Descriptors.MolLogP(mol) <= config['logp_filter']['threshold']
+    
+class LogPFilterForXMol(Filter):
+    def check(mol, conf):
+        @transform_linker_to_mol(conf)
+        def _check(mol, conf):
+            return LogPFilter.check(mol, conf)
+        return _check(mol, conf)
