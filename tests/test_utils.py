ee8e66dbea46abf768726757a862462068ec3da0
sishida21 <29348731+sishida21@users.noreply.github.com>
2025-08-16 18:49:57 +0900

    test: add a test for attach_fragment_to_all_sites decorator

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ import re
 import pytest
 from rdkit import Chem
 
-from chemtsv2.utils import transform_linker_to_mol
+from chemtsv2.utils import transform_linker_to_mol, attach_fragment_to_all_sites
 
 
 class TestTransformLinkerToMol:
@@ -101,3 +101,100 @@ class TestTransformLinkerToMol:
             TypeError, match="Check that this decorator is placed in the correct position."
         ):
             func(self.valid_linker_mol, self.valid_conf, "extra_arg")
+
+
+class TestAttachFragmentToAllSites:
+    def setup_method(self):
+        self.valid_conf = {"scaffold": "C1C([*:1])=CC([*:2])=CC=1[*:3]"}
+        self.invalid_conf = {}
+        self.valid_fragment_mol = Chem.MolFromSmiles("On1ccc(*)c1")
+        self.invalid_fragment_mol = Chem.MolFromSmiles("CC1*CCC(C)1")
+        self.two_attachment_points_mol = Chem.MolFromSmiles("On1ccc(*)c1*")
+
+    def test_attach_fragment_to_all_sites_invalid_mol(self):
+        @attach_fragment_to_all_sites(self.valid_conf)
+        def func(mol):
+            pass
+
+        with pytest.raises(
+            TypeError, match="Check this decorator is placed in the correct position."
+        ):
+            func("not_a_mol")
+
+    def test_attach_fragment_to_all_sites_missing_scaffold_key(self):
+        @attach_fragment_to_all_sites(self.invalid_conf)
+        def func(mol):
+            pass
+
+        with pytest.raises(
+            KeyError,
+            match="Must specify SMILES strings corresponding to the key `scaffold` in the config file.",
+        ):
+            func(self.valid_fragment_mol)
+
+    def test_attach_fragment_to_all_sites_wildcard_count_mismatch(self):
+        @attach_fragment_to_all_sites(self.valid_conf)
+        def func(mol):
+            pass
+
+        error_message = re.escape(
+            "The number of '*' in a smiles string must be 1. "
+            "Please set 'use_attachment_points_filter' to True and threshold to 1 in the configuration."
+        )
+        with pytest.raises(
+            ValueError,
+            match=error_message,
+        ):
+            func(self.two_attachment_points_mol)
+
+    def test_attach_fragment_to_all_sites_wildcard_count_mismatch_for_filter(self):
+        @attach_fragment_to_all_sites(self.valid_conf)
+        def func(mol, conf):
+            pass
+
+        result = func(self.two_attachment_points_mol, self.valid_conf)
+        assert not result, f"Expected output is `False` but got {result}"
+
+    def test_attach_fragment_to_all_sites_reward_molzip_failure(self):
+        @attach_fragment_to_all_sites(self.valid_conf)
+        def func(mol):
+            pass
+
+        result = func(self.invalid_fragment_mol)
+        assert result == -1, f"Expected output is `-1` but got {result}"
+
+    def test_attach_fragment_to_all_sites_filter_molzip_failure(self):
+        @attach_fragment_to_all_sites(self.valid_conf)
+        def func(mol, conf):
+            pass
+
+        result = func(self.invalid_fragment_mol, self.valid_conf)
+        assert not result, f"Expected output is `False` but got {result}"
+
+    def test_attach_fragment_to_all_sites_with_one_argument(self):  # for reward function
+        @attach_fragment_to_all_sites(self.valid_conf)
+        def func(mol):
+            return Chem.MolToSmiles(mol)
+
+        result = func(self.valid_fragment_mol)
+        assert result == "On1ccc(-c2cc(-c3ccn(O)c3)cc(-c3ccn(O)c3)c2)c1", f"Expected 'On1ccc(-c2cc(-c3ccn(O)c3)cc(-c3ccn(O)c3)c2)c1' but got {result}"
+
+    def test_attach_fragment_to_all_sites_with_two_argument(self):  # for filter function
+        @attach_fragment_to_all_sites(self.valid_conf)
+        def func(mol, conf):
+            return f"{Chem.MolToSmiles(mol)} {conf['scaffold']}"
+
+        result = func(self.valid_fragment_mol, self.valid_conf)
+        assert result == "On1ccc(-c2cc(-c3ccn(O)c3)cc(-c3ccn(O)c3)c2)c1 C1C([*:1])=CC([*:2])=CC=1[*:3]", (
+            f"Expected 'On1ccc(-c2cc(-c3ccn(O)c3)cc(-c3ccn(O)c3)c2)c1 C1C([*:1])=CC([*:2])=CC=1[*:3]' but got {result}"
+        )
+
+    def test_attach_fragment_to_all_sites_invalid_decorator_placement(self):
+        @attach_fragment_to_all_sites(self.valid_conf)
+        def func(mol, conf, extra_arg):
+            pass
+
+        with pytest.raises(
+            TypeError, match="Check that this decorator is placed in the correct position."
+        ):
+            func(self.valid_fragment_mol, self.valid_conf, "extra_arg")
